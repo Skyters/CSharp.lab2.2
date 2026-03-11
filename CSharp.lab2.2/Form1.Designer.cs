8fa5a15f0defbd8068b3bba8ed92edd4e7e50e13
Skyters <vlad.307123@gmail.com>
2026-03-11 10:47:34 +0800

    Добавлены новые элементы для GUI

--- CSharp.lab2.2/Form1.Designer.cs
+++ CSharp.lab2.2/Form1.Designer.cs
@@ -35,11 +35,14 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            buttonTask = new Button();
             SuspendLayout();
             // 
             // buttonStart
             // 
-            buttonStart.Location = new Point(12, 228);
+            buttonStart.Location = new Point(12, 297);
             buttonStart.Name = "buttonStart";
             buttonStart.Size = new Size(94, 29);
             buttonStart.TabIndex = 0;
@@ -49,7 +52,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 20);
+            label1.Location = new Point(12, 109);
             label1.Name = "label1";
             label1.Size = new Size(179, 20);
             label1.TabIndex = 1;
@@ -58,7 +61,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 86);
+            label2.Location = new Point(12, 174);
             label2.Name = "label2";
             label2.Size = new Size(483, 20);
             label2.TabIndex = 2;
@@ -67,7 +70,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 152);
+            label3.Location = new Point(12, 241);
             label3.Name = "label3";
             label3.Size = new Size(377, 20);
             label3.TabIndex = 3;
@@ -75,30 +78,58 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 43);
+            textBox1.Location = new Point(12, 132);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(201, 27);
             textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(12, 109);
+            textBox2.Location = new Point(12, 197);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(201, 27);
             textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(12, 175);
+            textBox3.Location = new Point(12, 264);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(201, 27);
             textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 56);
+            label4.Name = "label4";
+            label4.Size = new Size(118, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Число месяцев:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 79);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(60, 27);
+            textBox4.TabIndex = 8;
+            // 
+            // buttonTask
+            // 
+            buttonTask.Location = new Point(12, 12);
+            buttonTask.Name = "buttonTask";
+            buttonTask.Size = new Size(94, 29);
+            buttonTask.TabIndex = 9;
+            buttonTask.Text = "Задание";
+            buttonTask.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(584, 450);
+            Controls.Add(buttonTask);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -121,5 +152,8 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Button buttonTask;
     }
 }
