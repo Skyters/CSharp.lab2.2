10f2f09461b6875b88f915cd50c34c4cdf8b05e9
Skyters <vlad.307123@gmail.com>
2026-03-11 10:40:44 +0800

    Были добавлены элементы для GUI

--- CSharp.lab2.2/Form1.Designer.cs
+++ CSharp.lab2.2/Form1.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            buttonStart = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(12, 228);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(94, 29);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "Запуск";
+            buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Первоначальный взнос:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(483, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Граница превышения величины ежемесячного увеличения вклада:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(377, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Граница превышения размера вклада за n-месяцев:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(201, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 109);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(201, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 175);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(201, 27);
+            textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(584, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(buttonStart);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonStart;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
